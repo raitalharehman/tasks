1e12f747cca67310cab1562f042d8cefe49969a8
Rai Talha Rehman Khan <talharaay@gmail.com>
2019-09-12 09:42:50 +0500

    save

--- src/components/Home/devicestable.js
+++ src/components/Home/devicestable.js
@@ -2,63 +2,69 @@ import React from 'react';
 import MaterialTable from 'material-table';
 import { Paper } from '@material-ui/core';
 
-export default function DeviceTable() {
-    const [state, setState] = React.useState({
-        columns: [
-            { title: 'Name', field: 'name' },
-            { title: 'Warranty', field: 'warranty' },
-            { title: 'Expiry', field: 'expiry' },
-            { title: 'Cost', field: 'cost' },
-            { title: 'Image', field: 'image' },
-        ],
-        data: [
-            {
-                name: 'Mobile',
-                warranty: "1 Year",
-                expiry: '10 sep 2020',
-                cost: '100',
-                image: ""
-            }
-        ],
-    });
-
-    return (
-        <MaterialTable
-            title="Devices"
-            columns={state.columns}
-            data={state.data}
-            components={{
-                Container: props => <Paper {...props} elevation={0} />
-            }}
-            editable={{
-                onRowAdd: newData =>
-                    new Promise(resolve => {
-                        setTimeout(() => {
-                            resolve();
-                            const data = [...state.data];
-                            data.push(newData);
-                            setState({ ...state, data });
-                        }, 600);
-                    }),
-                onRowUpdate: (newData, oldData) =>
-                    new Promise(resolve => {
-                        setTimeout(() => {
-                            resolve();
-                            const data = [...state.data];
-                            data[data.indexOf(oldData)] = newData;
-                            setState({ ...state, data });
-                        }, 600);
-                    }),
-                onRowDelete: oldData =>
-                    new Promise(resolve => {
-                        setTimeout(() => {
-                            resolve();
-                            const data = [...state.data];
-                            data.splice(data.indexOf(oldData), 1);
-                            setState({ ...state, data });
-                        }, 600);
-                    }),
-            }}
-        />
-    );
+export default class DeviceTable extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            columns: [
+                { title: 'Name', field: 'name' },
+                { title: 'Warranty', field: 'warranty' },
+                { title: 'Expiry', field: 'expiry' },
+                { title: 'Cost', field: 'cost' },
+                { title: 'Image', field: 'image' },
+            ],
+            data: [
+                {
+                    name: 'Mobile',
+                    warranty: "1 Year",
+                    expiry: '10 sep 2020',
+                    cost: '100',
+                    image: ""
+                }
+            ],
+        }
+    }
+    render() {
+        const {columns, data} = this.state;
+        debugger
+        return (
+            <MaterialTable
+                title="Devices"
+                columns={columns}
+                data={data}
+                components={{
+                    Container: props => <Paper {...props} elevation={0} />
+                }}
+                editable={{
+                    onRowAdd: newData =>
+                        new Promise(resolve => {
+                            setTimeout(() => {
+                                resolve();
+                                let dataToAdd = this.state.data;
+                                dataToAdd.push(newData);
+                                this.setState({ data, dataToAdd });
+                            }, 600);
+                        }),
+                    onRowUpdate: (newData, oldData) =>
+                        new Promise(resolve => {
+                            setTimeout(() => {
+                                resolve();
+                                let dataToEdit = this.state.data;                                
+                                dataToEdit[dataToEdit.indexOf(oldData)] = newData;
+                                this.setState({ data, dataToEdit });
+                            }, 600);
+                        }),
+                    onRowDelete: oldData =>
+                        new Promise(resolve => {
+                            setTimeout(() => {
+                                resolve();
+                                let dataToDelete = this.state.data;
+                                dataToDelete.splice(dataToDelete.indexOf(oldData), 1);
+                                this.setState({ data, dataToDelete });                                
+                            }, 600);
+                        }),
+                }}
+            />
+        );
+    }
 }
